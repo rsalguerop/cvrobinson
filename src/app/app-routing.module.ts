37640c15dcfae7dac7ea1f9e0cc683c62311251c
Robinson <robsoncol1947@gmail.com>
2023-05-20 11:06:27 -0500

    add component and edit home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {SkillComponent} from "./skill/skill.component";
+import {ExperienceComponent} from "./experience/experience.component";
+import {HobbiesComponent} from "./hobbies/hobbies.component";
+import {AchievementsComponent} from "./achievements/achievements.component";
+import {EducationComponent} from "./education/education.component";
+import {SuggestionsComponent} from "./suggestions/suggestions.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+
+
+  {path : 'home', component: HomeComponent },
+  {path : 'skill', component: SkillComponent},
+  {path : 'experience' , component: ExperienceComponent},
+  {path : 'education', component: EducationComponent},
+  {path : 'achievements', component: AchievementsComponent },
+  {path : 'hobbies', component: HobbiesComponent},
+  {path : 'suggestions' , component: SuggestionsComponent},
+  {path : '**' , redirectTo : 'home' , pathMatch : 'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { SkillComponent } from './skill/skill.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
+import { AchievementsComponent } from './achievements/achievements.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
+import { SuggestionsComponent } from './suggestions/suggestions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SkillComponent,
+    ExperienceComponent,
+    EducationComponent,
+    AchievementsComponent,
+    HobbiesComponent,
+    SuggestionsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
